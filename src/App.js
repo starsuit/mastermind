e387db21478aecff0f337efba47a43640079fbd5
Samantha Dickeson <samlucydickeson@gmail.com>
2019-06-09 17:21:05 +0100

    set up initial state

--- src/App.js
+++ src/App.js
@@ -4,27 +4,30 @@ import "./App.css";
 // import components
 import Panel from "./Components/Panel";
 
+// import util functions
+import { generate } from "./utils/generateSequence";
+
 function App() {
-  const colours = [
-    "red",
-    "green",
-    "pink",
-    "blue",
-    "orange",
-    "purple",
-    "yellow"
-  ];
-  const answer = ["orange", "blue", "pink", "yellow"];
-  const guess = ["red", "green", "pink", "blue"];
-  const goes = Array.from({ length: 5 }, (_, i) => i);
+  const goes = Array.from({ length: 10 }, _ =>
+    Array.from({ length: 4 }, _ => "white")
+  );
+  const [pegArray, setPegArray] = React.useState(goes);
+  const [currentGuess, setCurrentGuess] = React.useState(0);
+
+  const answer = generate(4);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Mastermind</h1>
       </header>
       <main>
-        {goes.map(item => (
-          <Panel key={item} guess={guess} answer={answer} />
+        <Panel
+          key="answer"
+          guess={currentGuess > 9 ? answer : ["grey", "grey", "grey", "grey"]}
+        />
+        {pegArray.map((guess, i) => (
+          <Panel key={guess[0] + i} guess={guess} answer={answer} />
         ))}
       </main>
     </div>
--- src/Components/Panel.js
+++ src/Components/Panel.js
@@ -4,13 +4,13 @@ import Response from "./Response";
 import { checkGuess } from "../utils/checkGuess";
 
 const Panel = props => {
-  const response = checkGuess(props.guess, props.answer);
+  const response = !props.answer || checkGuess(props.guess, props.answer);
   return (
     <div data-testid="panel" className="panel">
       {props.guess.map((colour, i) => (
         <div key={colour + i} className={"peg " + colour} />
       ))}
-      <Response response={response} />
+      {!props.answer || <Response response={response} />}
     </div>
   );
 };
